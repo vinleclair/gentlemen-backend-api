cfdf20632dab5f7e8a7b4538dec11384fae0f3d8
vinleclair <vinleclair@gmail.com>
2020-04-10 23:55:40 -0400

    Add appointment create test

---
+++ tests/Gentlemen.IntegrationTests/Features/Appointments/AppointmentHelpers.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Gentlemen.Features.Appointments;
+
+namespace Gentlemen.IntegrationTests.Features.Appointments
+{
+    public static class AppointmentHelpers
+    {
+        public static async Task<Domain.Appointment> CreateAppointment(SliceFixture fixture, Create.Command command)
+        {
+            var dbContext = fixture.GetDbContext();
+
+            var appointmentCreateHandler = new Create.Handler(dbContext);
+            var created = await appointmentCreateHandler.Handle(command, new System.Threading.CancellationToken());
+
+            var dbAppointment = await fixture.ExecuteDbContextAsync(db => db.Appointments.Where(a => a.AppointmentId == created.Appointment.AppointmentId)
+                .SingleOrDefaultAsync());
+
+            return dbAppointment;
+        }
+    }
+}
---
+++ tests/Gentlemen.IntegrationTests/Features/Appointments/CreateTests.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using Gentlemen.Features.Appointments;
+using NUnit.Framework;
+
+namespace Gentlemen.IntegrationTests.Features.Appointments
+{
+    [TestFixture]
+    public class CreateTests : SliceFixture
+    {
+        [Test]
+        public async Task Expect_Create_Appointment()
+        {
+            var command = new Create.Command()
+            {
+                Appointment = new Create.AppointmentData()
+                {
+                    Name = "John",
+                    Email = "john.doe@example.com"
+                }
+            };
+
+            var appointment = await AppointmentHelpers.CreateAppointment(this, command);
+
+            Assert.NotNull(appointment);
+            Assert.AreEqual(appointment.Name, command.Appointment.Name);
+            Assert.AreEqual(appointment.Email, command.Appointment.Email);
+        }
+    }
+}
