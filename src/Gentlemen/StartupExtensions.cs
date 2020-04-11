7bbb9368324b4abedb41a9d14ea4b7d02725d9a3
vinleclair <vinleclair@gmail.com>
2020-04-10 21:11:18 -0400

    Fix Seril logging theme

--- src/Gentlemen/StartupExtensions.cs
+++ src/Gentlemen/StartupExtensions.cs
@@ -1,19 +1,16 @@
 ﻿using Microsoft.Extensions.Logging;
 using Serilog;
 using Serilog.Sinks.SystemConsole.Themes;
-
 namespace Gentlemen
 {
     public static class StartupExtensions
     {
         public static void AddSerilogLogging(this ILoggerFactory loggerFactory)
         {
-            // Attach the sink to the logger configuration
             var log = new LoggerConfiguration()
                 .MinimumLevel.Verbose()
                 .Enrich.FromLogContext()
-                //just for local debug
-                .WriteTo.Console(outputTemplate: "{Timestamp:HH:mm:ss} [{Level}] {SourceContext} {Message}{NewLine}{Exception}", theme: AnsiConsoleTheme.Code)
+                .WriteTo.Console(outputTemplate: "{Timestamp:HH:mm:ss} [{Level}] {SourceContext} {Message}{NewLine}{Exception}", theme: SystemConsoleTheme.Grayscale)
                 .CreateLogger();
 
             loggerFactory.AddSerilog(log);
