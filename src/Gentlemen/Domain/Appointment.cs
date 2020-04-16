3459cbceed95f27880004ca66a444b7d8178fa94
vinleclair <vinleclair@gmail.com>
2020-04-15 20:01:18 -0400

    Add service foreign key

--- src/Gentlemen/Domain/Appointment.cs
+++ src/Gentlemen/Domain/Appointment.cs
@@ -8,10 +8,11 @@ namespace Gentlemen.Domain
         public string ClientName { get; set; }
         public string ClientEmail { get; set; }
         public DateTime ScheduledDate { get; set; }
-        public int ServiceId { get; set; }
-        
-        
+
         public int BarberId { get; set; }
         public Barber Barber { get; set; }
+        
+        public int ServiceId { get; set; }
+        public Service Service { get; set; }
     }
 }
--- src/Gentlemen/Domain/Service.cs
+++ src/Gentlemen/Domain/Service.cs
@@ -1,13 +1,15 @@
-﻿namespace Gentlemen.Domain
+﻿using System.Collections.Generic;
+
+namespace Gentlemen.Domain
 {
     public class Service
     {
         public int ServiceId { get; set; }
 
         public string Name { get; set; }
-
         public int Price { get; set; }
-        
         public int Duration { get; set; }
+        
+        public List<Appointment> Appointments { get; set; }
     }
 }
